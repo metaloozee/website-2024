62dcb9c16d4f7553316300ea171d0a6cc06331f1
Ayan <ayanparkar123@gmail.com>
2024-08-04 16:05:19 +0530

    feat: new layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+"use client";
+
 import {
   Card,
   CardHeader,
@@ -11,108 +12,82 @@ import {
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 
-import { User, Globe, CodeXml } from "lucide-react";
+import { User, Globe, CodeXml, CornerDownLeft } from "lucide-react";
+
+import { motion } from "framer-motion";
+import MessageGroup from "@/components/Message";
 
 export default function IndexPage() {
   return (
-    <section className="px-4 container max-w-2xl space-y-14 py-10">
-      <div className="flex justify-start items-end gap-5">
-        <Avatar className="size-8">
-          <AvatarImage src="https://github.com/metaloozee.png" />
-          <AvatarFallback>A</AvatarFallback>
-        </Avatar>
-
-        <div className="flex flex-col gap-2">
-          <Card className="bg-zinc-900/50 flex flex-col max-w-fit">
-            <CardHeader className="p-4">
-              <p className="text-sm">hey there, im ayan</p>
-            </CardHeader>
-          </Card>
-          <Card className="bg-zinc-900/50 flex flex-col max-w-fit">
-            <CardHeader className="p-4">
-              <p className="text-sm">
-                im an eighteen-year-old programming enthusiast that is
-                passionate about both web development and artificial
-                intelligence. my drive comes from a never-ending curiosity.
-              </p>
-            </CardHeader>
-          </Card>
-          <Card className="bg-zinc-900/50 flex flex-col max-w-fit">
-            <CardHeader className="p-4">
-              <p className="text-sm italic">
-                (ask anything about me below or use the pre-made prompts)
-              </p>
-            </CardHeader>
-          </Card>
-        </div>
-      </div>
-
-      <div className="flex flex-row-reverse justify-start items-end gap-5">
-        <Avatar className="size-8">
-          <AvatarFallback>
-            <User className="size-4" />
-          </AvatarFallback>
-        </Avatar>
-
-        <div className="flex flex-col gap-2">
-          <Card className="flex flex-col">
-            <CardHeader className="p-4">
-              <p className="text-sm">what have you made so far?</p>
-            </CardHeader>
-          </Card>
-        </div>
-      </div>
-
-      <div className="flex justify-start items-end gap-5">
-        <Avatar className="size-8">
-          <AvatarImage src="https://github.com/metaloozee.png" />
-          <AvatarFallback>A</AvatarFallback>
-        </Avatar>
-
-        <div className="flex flex-col gap-2">
-          <Card className="bg-zinc-900/50 flex flex-col max-w-fit">
-            <CardHeader className="p-4">
-              <p className="text-sm">
-                here are some of my cool projects -
-              </p>
-            </CardHeader>
-          </Card>
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
-            <Card className="bg-zinc-900/50 flex flex-col justify-between max-w-xs">
-              <CardHeader className="p-4">
-                <CardTitle>quickvid</CardTitle>
-                <CardDescription className="text-xs">
-                    quickvid is an open sourced web application in which one can
-                    summarize hour-long youtube videos, verify the authenticity
-                    of the video and chat with it based on the video&apos;s
-                    context.
-                </CardDescription>
-              </CardHeader>
-              <CardFooter className="px-4">
-                <Button className="w-full">
-                  <Globe className="size-5" />
-                </Button>
-              </CardFooter>
-            </Card>
-
-            <Card className="bg-zinc-900/50 flex flex-col justify-between max-w-xs">
-              <CardHeader className="p-4">
-                <CardTitle>learnify</CardTitle>
-                <CardDescription className="text-xs">
-                    learnify is a personalized learning platform designed to
-                    enhance the learning experience for both teachers and
-                    students.
-                </CardDescription>
-              </CardHeader>
-              <CardFooter className="px-4">
-                <Button className="w-full">
-                  <CodeXml className="size-5" />
-                </Button>
-              </CardFooter>
-            </Card>
-          </div>
-        </div>
-      </div>
+    <section className="relative px-4 h-screen flex items-center justify-center container max-w-2xl space-y-14 py-10">
+      <motion.ul
+        transition={{
+          staggerChildren: 0.3,
+          delayChildren: 0.3,
+        }}
+        initial="hidden"
+        animate="show"
+      >
+        <MessageGroup
+          messages={[
+            {
+              key: "intro",
+              content: <>hey there, im ayan</>,
+            },
+            {
+              key: "short-about-me",
+              content: (
+                <>
+                  im an eighteen-year-old programming enthusiast that is
+                  passionate about both web development and artificial
+                  intelligence. my drive comes from a never-ending curiosity.
+                </>
+              ),
+            },
+            {
+              key: "cards",
+              isGroup: true,
+              content: (
+                <div className="flex gap-2 flex-wrap">
+                  <Button
+                    className="cursor-pointer"
+                    variant={"secondary"}
+                    asChild
+                  >
+                    <Card className="bg-zinc-900/50 flex flex-col max-w-fit">
+                      <CardHeader className="p-4">
+                        <p className="text-sm italic">about-me</p>
+                      </CardHeader>
+                    </Card>
+                  </Button>
+                  <Button
+                    className="cursor-pointer"
+                    variant={"secondary"}
+                    asChild
+                  >
+                    <Card className="bg-zinc-900/50 flex flex-col max-w-fit">
+                      <CardHeader className="p-4">
+                        <p className="text-sm italic">what-have-i-done</p>
+                      </CardHeader>
+                    </Card>
+                  </Button>
+                  <Button
+                    className="cursor-pointer"
+                    variant={"secondary"}
+                    asChild
+                  >
+                    <Card className="bg-zinc-900/50 flex flex-col max-w-fit">
+                      <CardHeader className="p-4">
+                        <p className="text-sm italic">how-do-you-contact-me</p>
+                      </CardHeader>
+                    </Card>
+                  </Button>
+                </div>
+              ),
+            },
+          ]}
+        />
+      </motion.ul>
     </section>
   );
 }
---
+++ src/components/Message.tsx
@@ -0,0 +1,84 @@
+"use client";
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardFooter,
+  CardDescription,
+  CardContent,
+} from "@/components/ui/card";
+import { motion } from "framer-motion";
+import { ReactNode } from "react";
+
+const groupVariant = {
+  hidden: { opacity: 0, x: -5 },
+  show: { opacity: 1, x: 0 },
+};
+const itemVariant = {
+  hidden: { opacity: 0 },
+  show: { opacity: 1 },
+};
+
+function MessageBubble({
+  isGroup,
+  content,
+}: {
+  isGroup?: boolean;
+  content: ReactNode;
+}) {
+  return (
+    <motion.div
+      transition={{
+        type: "spring",
+        mass: 1,
+        damping: 100,
+        stiffness: 500,
+      }}
+      variants={itemVariant}
+    >
+      {isGroup ? (
+        <div>{content}</div>
+      ) : (
+        <Card className="bg-zinc-900/50 flex flex-col max-w-fit">
+          <CardHeader className="p-4">
+            <p className="text-sm">{content}</p>
+          </CardHeader>
+        </Card>
+      )}
+    </motion.div>
+  );
+}
+
+export default function MessageGroup({
+  messages,
+}: {
+  messages: Array<{ key: string; content: ReactNode, isGroup?: boolean }>;
+}) {
+  return (
+    <motion.li
+      transition={{
+        type: "spring",
+        mass: 11,
+        damping: 140,
+        stiffness: 500,
+
+        staggerChildren: 0.1,
+      }}
+      variants={groupVariant}
+      className="flex gap-3 md:gap-5 justify-start items-end"
+    >
+      <Avatar>
+        <AvatarImage src="https://github.com/metaloozee.png" />
+        <AvatarFallback>A</AvatarFallback>
+      </Avatar>
+
+      <div className="flex flex-col gap-2">
+        {messages.map(({ key: id, content, isGroup }, i) => (
+          <MessageBubble key={id} content={content} isGroup={isGroup} />
+        ))}
+      </div>
+    </motion.li>
+  );
+}
